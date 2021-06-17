7aaedbc5ac2c9fa5d4a6c1054e7184f9a96d9824
Brandon McGinnis <brandon@smokeandswine.co>
2021-06-16 23:09:20 -0400

    Changed the way the Expected Ship Date: is extracted from the Comment. Also added the method to update the shipdate_expected field with the Expected Ship Date: from the Comment

--- includes/functions.php
+++ includes/functions.php
@@ -5,23 +5,42 @@
     }
 
     // Extract and format the Expected Ship Date from the comment
+    function extractShipDate($comment) {
+        $string = 'Expected Ship Date: ';
+        if (strripos($comment,$string)) {
+            $expectedDate = substr($comment, strripos($comment,$string)+strlen($string),8);
+            if(checkIsValidDate($expectedDate)) {
+                return $expectedDate;
+            }
+        }
+    }
+
+    // Format the Expected Ship Date for the database
     function formatShipDate($dateString) {
-        $formattedShipDate = date("Y-m-d", strtotime($dateString)); 
+        $formattedShipDate = date("Y-m-d H:i:s", strtotime($dateString)); 
         return $formattedShipDate;
     }
 
     // Format the Comment and extract the Ship Date
     function formatComment($comment) {
+        $expectedDate = extractShipDate($comment);
+        if(checkIsValidDate($expectedDate) !== false) {
+            $strReplaced = str_replace("Expected Ship Date: " . $expectedDate, "", $comment);
             return $strReplaced . "<br/><br/><em>Expected Ship Date: " . extractShipDate($comment) . "</em>";
         } else {
             return $comment;
         }
     }
 
-    
-    function updateShipDateField ($order, $dateString) {
-        $formattedShipDate = formatShipDate($dateString);
-        $updateSQL = "UPDATE comments SET shipdate_expected=" . $formattedShipDate . " WHERE orderid=" . $order;
+    // Update the shipdate_expected field in the database with Expected Ship Date from the comment
+    function updateExpectedShipDateField($conn, $orderid, $comment) {
+        $formattedShipDate = formatShipDate(extractShipDate($comment));
+        $updateSQL = "UPDATE sweetwater_test SET shipdate_expected='" . $formattedShipDate . "' WHERE orderid=" . $orderid;
+        if ($conn->query($updateSQL) === true) {
+            echo "Record updated successfully";
+          } else {
+            echo "Error updating record: " . $conn->error;
+          }
     }
 
 ?>
--- index.php
+++ index.php
@@ -12,7 +12,14 @@
         $resCandyComments = $conn->query($qCommentsForCandy);
         if($resCandyComments->num_rows > 0) {
 
-            while($candyComment = $resCandyComments->fetch_assoc()) { ?>
+            while($candyComment = $resCandyComments->fetch_assoc()) { 
+                if ($candyComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($candyComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$candyComment["orderid"],$candyComment["comments"]);
+                    }
+                }  
+                ?>
                 <div class="card"> 
                     <?php echo "#" . $candyComment["orderid"]; ?>
                     <div class="card-body">
@@ -21,6 +28,7 @@
                 </div>
                 <?php
             }
+
         } else {
             echo "0 results";
         }
@@ -32,6 +40,12 @@
         if($resCallComments->num_rows > 0) {
 
             while($callComment = $resCallComments->fetch_assoc()) {
+                if ($callComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($callComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$callComment["orderid"],$callComment["comments"]);
+                    }
+                }  
             ?>
                 <div class="card">
                 <?php echo "#" . $callComment["orderid"] ?>
@@ -48,11 +62,17 @@
         ?>
 <h2 class="my-4" id="dont-call-me">Don't Call Me comments (<a href="#top">back to top</a>)</h2>
     <?php
-        $qCommentsForDontCall= "SELECT * FROM `sweetwater_test` WHERE `comments` LIKE '%don%t call me%' OR `comments` LIKE '%Do not call%'";
+        $qCommentsForDontCall = "SELECT * FROM `sweetwater_test` WHERE `comments` LIKE '%don%t call me%' OR `comments` LIKE '%Do not call%'";
         $resDontCallComments = $conn->query($qCommentsForDontCall);
         if($resDontCallComments->num_rows > 0) {
             
             while($dontCallComment = $resDontCallComments->fetch_assoc()) {
+                if ($dontCallComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($dontCallComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$dontCallComment["orderid"],$dontCallComment["comments"]);
+                    }
+                }  
                 ?>
                 <div class="card">
                 <?php echo "#" . $dontCallComment["orderid"] ?>
@@ -73,6 +93,12 @@
         $resReferredComments = $conn->query($qCommentsForRefered);
         if($resReferredComments->num_rows > 0) {
             while($referredComment = $resReferredComments->fetch_assoc()) {
+                if ($referredComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($referredComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$referredComment["orderid"],$referredComment["comments"]);
+                    }
+                }  
                 ?>
                 <div class="card">
                     <?php echo "#" . $referredComment["orderid"] ?>
@@ -94,6 +120,12 @@
         if($resSignatureComments->num_rows > 0) {
 
             while($signatureComment = $resSignatureComments->fetch_assoc()) {
+                if ($signatureComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($signatureComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$signatureComment["orderid"],$signatureComment["comments"]);
+                    }
+                }  
                 ?>
                 <div class="card">
                     <?php echo "#" . $signatureComment["orderid"] ?>
@@ -101,7 +133,7 @@
                     <?php echo formatComment($signatureComment["comments"]); ?>
                     </div>
                 </div>
-                <?php 
+                <?php  
             }
 
         } else {
@@ -115,6 +147,12 @@
         if($resEverythingElse->num_rows > 0) {
 
             while($otherComment = $resEverythingElse->fetch_assoc()) {
+                if ($otherComment["shipdate_expected"] == '0000-00-00 00:00:00') {
+                    $expectedShipDate = extractShipDate($otherComment["comments"]);
+                    if(checkIsValidDate($expectedShipDate)) {
+                        updateExpectedShipDateField($conn,$otherComment["orderid"],$otherComment["comments"]);
+                    }
+                }  
                 ?>
                 <div class="card">
                     <?php echo "#" . $otherComment["orderid"] ?>
